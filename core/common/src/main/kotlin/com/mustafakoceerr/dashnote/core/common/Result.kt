feca3ead621bd5b2389c36570edfe6c292e1b275
MustafaKoceerr <mustafakoceerr@hotmail.com>
2026-04-10 22:25:57 +0300

    core/common: add shared result and error abstractions

--- core/common/src/main/kotlin/com/mustafakoceerr/dashnote/core/common/Result.kt
+++ core/common/src/main/kotlin/com/mustafakoceerr/dashnote/core/common/Result.kt
@@ -1,2 +1,28 @@
 package com.mustafakoceerr.dashnote.core.common
 
+import com.mustafakoceerr.dashnote.core.common.error.RootError
+
+typealias EmptyResult<E> = Result<Unit, E>
+
+sealed interface Result<out D, out E : RootError> {
+    data class Success<out D, out E : RootError>(val data: D) : Result<D, E>
+    data class Error<out D, out E : RootError>(val error: E) : Result<D, E>
+}
+
+// Data veya Domain katmanında veriyi hızlıca map'lemek için pragmatik extension'lar
+inline fun <T, E : RootError, R> Result<T, E>.map(map: (T) -> R): Result<R, E> {
+    return when (this) {
+        is Result.Error -> Result.Error(error)
+        is Result.Success -> Result.Success(map(data))
+    }
+}
+
+inline fun <T, E : RootError> Result<T, E>.onSuccess(action: (T) -> Unit): Result<T, E> {
+    if (this is Result.Success) action(data)
+    return this
+}
+
+inline fun <T, E : RootError> Result<T, E>.onError(action: (E) -> Unit): Result<T, E> {
+    if (this is Result.Error) action(error)
+    return this
+}
--- core/common/src/main/kotlin/com/mustafakoceerr/dashnote/core/common/UiText.kt
+++ core/common/src/main/kotlin/com/mustafakoceerr/dashnote/core/common/UiText.kt
@@ -1,2 +1,27 @@
 package com.mustafakoceerr.dashnote.core.common
 
+import android.content.Context
+import androidx.annotation.StringRes
+
+/**
+ * ViewModel'dan UI'a metin gönderirken Context sızıntısını (Memory Leak) ve
+ * Android bağımlılığını engellemek için kullanılan wrapper sınıf.
+ */
+sealed class UiText {
+    data class DynamicString(val value: String) : UiText()
+
+    class StringResource(
+        @StringRes val resId: Int,
+        vararg val args: Any
+    ) : UiText()
+
+    /**
+     * UI katmanında bu metni çözmek için çağrılacak fonksiyon.
+     */
+    fun asString(context: Context): String {
+        return when (this) {
+            is DynamicString -> value
+            is StringResource -> context.getString(resId, *args)
+        }
+    }
+}
